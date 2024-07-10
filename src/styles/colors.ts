a2c8afe202c54c48ffb24845a292f8d3d175922a
Eduardo Sávio <saviodudu79@gmail.com>
2024-07-09 21:50:01 -0300

    feat: colors palette

---
+++ src/styles/colors.ts
@@ -0,0 +1,54 @@
+//TODO: Add color palette
+export const colors = {
+  primary: {
+    DEFAULT: "#00666C",
+    extradark: "#",
+    dark: "#",
+    light: "#",
+    extralight: "#",
+  },
+  secondary: {
+    DEFAULT: "#",
+    extradark: "#",
+    dark: "#",
+    light: "#",
+    extralight: "#",
+  },
+  tertiary: {
+    DEFAULT: "#",
+    extradark: "#",
+    dark: "#",
+    light: "#",
+    extralight: "#",
+  },
+  success: {
+    DEFAULT: "#",
+    extradark: "#",
+    dark: "#",
+    light: "#",
+    extralight: "#",
+  },
+  warning: {
+    DEFAULT: "#",
+    extradark: "#",
+    dark: "#",
+    light: "#",
+    extralight: "#",
+  },
+  danger: {
+    DEFAULT: "#",
+    extradark: "#",
+    dark: "#",
+    light: "#",
+    extralight: "#",
+  },
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: {
+    DEFAULT: "#",
+    extradark: "#",
+    dark: "#",
+    light: "#",
+    extralight: "#",
+  },
+};
